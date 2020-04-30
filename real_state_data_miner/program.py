f14f0f3a30c9685af3649d6ee04152e25bcde587
irmoralesb <irmoralesb@hotmail.com>
2020-04-29 21:18:23 -0500

    Real State Data Miner (Completed)

--- real_state_data_miner/program.py
+++ real_state_data_miner/program.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import statistics
+
 from data_types import Purchase
 
 
@@ -56,7 +58,53 @@ def query_data(data):
     low_purchase = data[0]
     print("The least expensive house is ${:,}".format(data[0].price))
     # average price house
+    # USING List Comprehensions
+    prices = [
+        p.price  # projection or items
+        for p in data  # The set to process
+    ]
+    mean_price = statistics.mean(prices)
+    print("The average house price is ${:,}".format(int(mean_price)))
     # average price of 2 bedroom house?
+    two_bed_homes = [
+        p
+        for p in data
+        if p.beds == 2
+    ]
+    # List comprehensions
+    ave_price = statistics.mean([p.price for p in two_bed_homes])
+    ave_baths = statistics.mean([p.baths for p in two_bed_homes])
+    ave_sqft = statistics.mean([p.sq__ft for p in two_bed_homes])
+    print(
+        "The average 2 bedrooms house price is ${:,}, baths={}, sq_ft={:,}".format(int(ave_price), round(ave_baths, 1),
+
+                                                                                   round(ave_sqft, 1)))
+
+    # Same sample but using Generator Expression
+    # ONE ITEM PROCESSED AT A TIME
+    two_bed_homes = (
+        p
+        for p in data
+        if p.beds == 2
+    )
+    homes = []
+    for h in two_bed_homes:
+        if len(homes) > 5:
+            break
+        homes.append(h)
+
+    ave_price = statistics.mean((announce(p.price, "price") for p in homes))
+    ave_baths = statistics.mean((announce(p.baths, "baths") for p in homes))
+    ave_sqft = statistics.mean((announce(p.sq__ft, "square feet") for p in homes))
+    print(
+        "The average 2 bedrooms house price is ${:,}, baths={}, sq_ft={:,}".format(int(ave_price), round(ave_baths, 1),
+
+                                                                                   round(ave_sqft, 1)))
+
+
+def announce(item, msg):
+    print("Pulling item {} for {}".format(item, msg))
+    return item
 
 
 if __name__ == "__main__":
